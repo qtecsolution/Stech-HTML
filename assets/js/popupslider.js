d5df2e568ce05972fdb3c016edef1833ef7c1eaa
it27472 <istiak.tushar.qtec@gmail.com>
2022-02-03 19:14:41 +0600

    contact completed

---
+++ assets/js/popupslider.js
@@ -0,0 +1,17 @@
+// window.addEventListener('load', function() {
+// });
+
+
+baguetteBox.run('.gallery', {
+    captions: true,
+    buttons: true, // arrows navigation
+    fullScreen: false,
+    noScrollbars: false,
+    titleTag: true,
+    // bodyClass: 'baguetteBox-open',
+    titleTag: false,
+    async: false,
+    preload: 2,
+    animation: 'slideIn', // fadeIn or slideIn
+    overlayBackgroundColor: 'rgba(0,0,0,.75)'
+});
