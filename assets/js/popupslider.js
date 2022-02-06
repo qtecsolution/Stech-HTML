d51ee4d81e80bb08b96d37f4098f6c9e78ed2549
it27427 <istiak.tushar.qtec@gmail.com>
2022-02-06 19:07:12 +0600

    working on gallery

--- assets/js/popupslider.js
+++ assets/js/popupslider.js
@@ -15,3 +15,31 @@ baguetteBox.run('.gallery', {
     animation: 'slideIn', // fadeIn or slideIn
     overlayBackgroundColor: 'rgba(0,0,0,.75)'
 });
+
+baguetteBox.run('.gallerystech-1', {
+    captions: true,
+    buttons: true, // arrows navigation
+    fullScreen: false,
+    noScrollbars: false,
+    titleTag: true,
+    // bodyClass: 'baguetteBox-open',
+    titleTag: false,
+    async: false,
+    preload: 2,
+    animation: 'slideIn', // fadeIn or slideIn
+    overlayBackgroundColor: 'rgba(0,0,0,.75)'
+});
+
+baguetteBox.run('.gallerystech-2', {
+    captions: true,
+    buttons: true, // arrows navigation
+    fullScreen: false,
+    noScrollbars: false,
+    titleTag: true,
+    // bodyClass: 'baguetteBox-open',
+    titleTag: false,
+    async: false,
+    preload: 2,
+    animation: 'slideIn', // fadeIn or slideIn
+    overlayBackgroundColor: 'rgba(0,0,0,.75)'
+});
