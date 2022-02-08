1ec468659b4cc4e4c001f6429d83d32495f0f00b
it27472 <istiak.tushar.qtec@gmail.com>
2022-02-08 16:05:09 +0600

    working on animation

---
+++ assets/js/animation.js
@@ -0,0 +1,9 @@
+wow = new WOW({
+    boxClass:     'wow',      // default
+    animateClass: 'animate__animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+})
+
+wow.init();
