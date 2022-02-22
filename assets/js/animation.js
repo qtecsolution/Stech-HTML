4049a4e6d463f737171c8daba719e239d7d13ee9
it27472 <istiak.tushar.qtec@gmail.com>
2022-02-22 16:39:29 +0600

    working on gsap

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,9 +1,32 @@
-wow = new WOW({
-    boxClass:     'wow',      // default
-    animateClass: 'animate__animated', // default
-    offset:       0,          // default
-    mobile:       true,       // default
-    live:         true        // default
-})
+// TweenLite.from('.stechdigitalribbon',{y:'100%'})
+// var height = new TimelineMax({repeat: 0, yoyo: true, repeatDelay: 2})
+// .to('.stechdigitalribbon', 1, {y: '0'})
 
-wow.init();
+// TweenLite.from('.stechdigital-title',{x:'-100%'})
+// var lines = new TimelineMax({repeat: 0, yoyo: true, repeatDelay: 3})
+// .to('.stechdigital-title', 2, {x: '0%'})
+
+gsap.from('.stechdigitalribbon', {
+    y: -20,
+    scaleY: 0,
+    duration: 2,
+    ease: 'back',
+    stagged: 1
+});
+
+gsap.to('.stechdigitalribbon', {
+    y: 0,
+    scaleY: 1
+});
+
+gsap.from('.stechdigital-title', {
+    x: '-100%',
+    duration: 3,
+    delay: 2,
+    ease: 'back',
+    stagged: 2
+});
+
+gsap.to('.stechdigital-title', {
+    x: 0
+});
