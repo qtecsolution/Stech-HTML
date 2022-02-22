bde85dbb36c9332db18c0dc49a993856e03649b2
it27472 <istiak.tushar.qtec@gmail.com>
2022-02-22 18:00:06 +0600

    home animation initial done

---
+++ assets/js/animations/home-animation.js
@@ -0,0 +1,224 @@
+
+// STECH-DIGITAL ANIMATION START
+
+gsap.from('.stechdigitalribbon', {
+    duration: 2,
+    ease: 'power2.in',
+    scaleY: 0,
+    opacity: 0
+});
+
+gsap.to('.stechdigitalribbon', {
+    scaleY: 1
+});
+
+gsap.from('.stechdigital-title', {
+    x: '-40',
+    duration: 2.25,
+    delay: 2.25,
+    ease: 'back',
+    opacity: 0
+});
+
+gsap.to('.stechdigital-title', {
+    x: 0
+});
+
+gsap.from('.setchdigital-text', {
+    duration: 2.5,
+    delay: 2.5,
+    y: 100,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.setchdigital-text', {
+    y: 0
+});
+// STECH-DIGITAL ANIMATION END
+
+
+// OUR-CONCERN ANIMATION START
+gsap.from('.ourconcernribbon', {
+    duration: 2,
+    ease: 'power2.in',
+    scaleY: 0,
+    opacity: 0
+});
+
+gsap.to('.ourconcernribbon', {
+    scaleY: 1
+});
+
+gsap.from('.ourconcern-title', {
+    x: '-40',
+    duration: 2.25,
+    delay: 2.25,
+    ease: 'back',
+    opacity: 0
+});
+
+gsap.to('.ourconcern-title', {
+    x: 0
+});
+
+gsap.from('.ourconcern-text', {
+    duration: 2.5,
+    delay: 2.5,
+    y: 100,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.ourconcern-text', {
+    y: 0
+});
+// OUR-CONCERN ANIMATION END
+
+// OUR-SERVICE ANIMATION START
+gsap.from('.serviceribbon', {
+    duration: 2,
+    ease: 'power2.in',
+    scaleY: 0,
+    opacity: 0
+});
+
+gsap.to('.serviceribbon', {
+    scaleY: 1
+});
+
+gsap.from('.service-title', {
+    x: '-40',
+    duration: 2.25,
+    delay: 2.25,
+    ease: 'back',
+    opacity: 0
+});
+
+gsap.to('.service-title', {
+    x: 0
+});
+
+gsap.from('.service-text', {
+    duration: 2.5,
+    delay: 2.5,
+    y: 100,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.service-text', {
+    y: 0
+});
+// OUR-SERVICE ANIMATION END
+
+// OUR-CLIENTS ANIMATION START
+gsap.from('.ourclientsribbon', {
+    duration: 2,
+    ease: 'power2.in',
+    scaleY: 0,
+    opacity: 0
+});
+
+gsap.to('.ourclientsribbon', {
+    scaleY: 1
+});
+
+gsap.from('.ourclients-title', {
+    x: '-40',
+    duration: 2.25,
+    delay: 2.25,
+    ease: 'back',
+    opacity: 0
+});
+
+gsap.to('.ourclients-title', {
+    x: 0
+});
+
+gsap.from('.ourclients-text', {
+    duration: 2.5,
+    delay: 2.5,
+    y: 100,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.ourclients-text', {
+    y: 0
+});
+// OUR-CLIENTS ANIMATION END
+
+// COMPANY-VALUES ANIMATION START
+gsap.from('.companyvaluesribbon', {
+    duration: 2,
+    ease: 'power2.in',
+    scaleY: 0,
+    opacity: 0
+});
+
+gsap.to('.companyvaluesribbon', {
+    scaleY: 1
+});
+
+gsap.from('.companyvalues-title', {
+    x: '-40',
+    duration: 2.25,
+    delay: 2.25,
+    ease: 'back',
+    opacity: 0
+});
+
+gsap.to('.companyvalues-title', {
+    x: 0
+});
+
+gsap.from('.companyvalue-text', {
+    duration: 2.5,
+    delay: 2.5,
+    y: 100,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.companyvalue-text', {
+    y: 0
+});
+// COMPANY-VALUES ANIMATION END
+
+// OUR-STRATIGY ANIMATION START
+gsap.from('.strategiesribbon', {
+    duration: 2,
+    ease: 'power2.in',
+    scaleY: 0,
+    opacity: 0
+});
+
+gsap.to('.strategiesribbon', {
+    scaleY: 1
+});
+
+gsap.from('.strategies-title', {
+    x: '-40',
+    duration: 2.25,
+    delay: 2.25,
+    ease: 'back',
+    opacity: 0
+});
+
+gsap.to('.strategies-title', {
+    x: 0
+});
+
+gsap.from('.strategies-text', {
+    duration: 2.5,
+    delay: 2.5,
+    y: 100,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.strategies-text', {
+    y: 0
+});
+// OUR-STRATIGY ANIMATION END
