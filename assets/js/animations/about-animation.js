b31e3072fa170ffc4428d7454fbb27a54bb19a15
it27472 <istiak.tushar.qtec@gmail.com>
2022-02-23 14:35:02 +0600

    about animation done

---
+++ assets/js/animations/about-animation.js
@@ -0,0 +1,262 @@
+// STECH AESTHETICS ANIMATION START
+gsap.from('.stechaestheticsribbon', {
+    duration: 0.5,
+    ease: 'power2.in',
+    scaleY: 0,
+    opacity: 0
+});
+
+gsap.to('.stechaestheticsribbon', {
+    scaleY: 1
+});
+
+gsap.from('.stechaesthetics-title', {
+    duration: 0.75,
+    delay: 0.75,
+    y: 40,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.stechaesthetics-title', {
+    y: 0
+});
+
+gsap.from('.stechaesthetics-text', {
+    duration: 1,
+    delay: 1,
+    y: 20,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.stechaesthetics-text', {
+    y: 0
+});
+
+// STECH AESTHETICS ANIMATION END
+
+// CHAIRMAN-MESSAGE ANIMATION START
+gsap.from('.chairmanmessageribbon', {
+    duration: 1.25,
+    ease: 'power2.in',
+    scaleY: 0,
+    opacity: 0
+});
+
+gsap.to('.chairmanmessageribbon', {
+    scaleY: 1
+});
+
+gsap.from('.chairmanmessage-title', {
+    x: '-40',
+    duration: 1.5,
+    delay: 1.5,
+    ease: 'back',
+    opacity: 0
+});
+
+gsap.to('.chairmanmessage-title', {
+    x: 0
+});
+
+gsap.from('.chairmanmessage-text', {
+    duration: 1.75,
+    delay: 1.75,
+    y: 40,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.chairmanmessage-text', {
+    y: 0
+});
+
+gsap.from('.chairman-profile', {
+    duration: 2,
+    delay: 2,
+    x: -20,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.chairman-profile', {
+    x: 0
+});
+
+gsap.from('.chairmaninfo .name', {
+    duration: 2,
+    delay: 2,
+    y: 20,
+    ease: 'power2.inOut',
+    opacity: 0,
+    stagger: 0.05
+});
+
+gsap.to('.chairmaninfo', {
+    y: 0
+});
+
+gsap.from('.chairmaninfo .post', {
+    duration: 2,
+    delay: 2,
+    y: -20,
+    ease: 'power2.inOut',
+    opacity: 0,
+    stagger: 0.05
+});
+
+gsap.to('.chairmaninfo', {
+    y: 0
+});
+
+gsap.from('.breaf-text', {
+    duration: 2,
+    delay: 2,
+    x: 20,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.breaf-text', {
+    x: 0
+});
+
+// CHAIRMAN-MESSAGE ANIMATION END
+
+// CORPORATE-DIRECTORS ANIMATION START
+gsap.from('.corporatedirectorsribbon', {
+    duration: 2.25,
+    ease: 'power2.in',
+    scaleY: 0,
+    opacity: 0
+});
+
+gsap.to('.corporatedirectorsribbon', {
+    scaleY: 1
+});
+
+gsap.from('.corporatedirectors-title', {
+    x: '-40',
+    duration: 2.5,
+    delay: 2.5,
+    ease: 'back',
+    opacity: 0
+});
+
+gsap.to('.corporatedirectors-title', {
+    x: 0
+});
+
+gsap.from('.corporatedirectors-text', {
+    duration: 2.75,
+    delay: 2.75,
+    y: 20,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.corporatedirectors-text', {
+    y: 0
+});
+
+gsap.from('.corporatedirector', {
+    duration: 3,
+    delay: 3,
+    y: 20,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.corporatedirector', {
+    y: 0
+});
+
+// CORPORATE-DIRECTORS ANIMATION END
+
+
+// CMISSION-VISION ANIMATION START
+
+gsap.from('.missionvisionribbon', {
+    duration: 3.25,
+    ease: 'power2.in',
+    scaleY: 0,
+    opacity: 0
+});
+
+gsap.to('.missionvisionribbon', {
+    scaleY: 1
+});
+
+gsap.from('.missionvision-title', {
+    x: '-40',
+    duration: 3.5,
+    delay: 3.5,
+    ease: 'back',
+    opacity: 0
+});
+
+gsap.to('.missionvision-title', {
+    x: 0
+});
+
+gsap.from('.missionvision-text', {
+    duration: 3.75,
+    delay: 3.75,
+    y: 20,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.missionvision-text', {
+    y: 0
+});
+
+gsap.from('.missioninfo', {
+    duration: 3.8,
+    delay: 3.8,
+    x: -20,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.missioninfo', {
+    x: 0
+});
+
+gsap.from('.mission-thumbnail', {
+    duration: 3.9,
+    delay:3.9,
+    x: 20,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.mission-thumbnail', {
+    x: 0
+});
+
+gsap.from('.vision-thumbnail', {
+    duration: 4,
+    delay: 4,
+    x: -20,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.vision-thumbnail', {
+    x: 0
+});
+
+gsap.from('.visioninfo', {
+    duration: 4,
+    delay: 4,
+    x: 20,
+    ease: 'power2.inOut',
+    opacity: 0
+});
+
+gsap.to('.visioninfo', {
+    x: 0
+});
+// CMISSION-VISION ANIMATION END
