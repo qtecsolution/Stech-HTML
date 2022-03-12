782de724691e8e6fc3470b449cb5c2cca5fccb6f
it27427 <istiak.tushar.qtec@gmail.com>
2022-03-12 19:41:37 +0600

    test

---
+++ assets/js/animations/about.js
@@ -0,0 +1,25 @@
+// let controller = new ScrollMagic.Controller();
+
+// let scene = new ScrollMagic.Scene({
+//     triggerElement: '.stechaesthetics'
+// })
+// .setClassToggle('.big-title-1', 'changed')
+// .addTo(controller)
+
+// .setTween('scene-2 h2', {
+//     y: '40'
+// })
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('#scene-2', {
+    scrollTrigger: {
+        trigger: '.stechaesthetics',
+        start: 'top center',
+        marker: true,
+        toggleActions: 'play none reverse none'
+    },
+    y: 70.8 + 'vh',
+    // rotation: 360,
+    duration: 2
+});
--- assets/js/pageloader.js
+++ assets/js/pageloader.js
@@ -2,6 +2,42 @@ const preloader = document.querySelector('#preloader');
 
 window.addEventListener('load', showHidePreloader);
 
+gsap.from('#preloadpath-1', {
+    duration: 1,
+    delay: 1,
+    ease: 'power2.in',
+    y: 100,
+    opacity: 0
+});
+
+gsap.to('#preloadpath-1', {
+    y: 0
+});
+
+gsap.from('#preloadpath-2', {
+    duration: 1,
+    delay: 1,
+    ease: 'power2.in',
+    y: 100,
+    opacity: 0
+});
+
+gsap.to('#preloadpath-2', {
+    y: 0
+});
+
+gsap.from('#preloadpath-3', {
+    duration: 1,
+    delay: 1,
+    ease: 'power2.in',
+    y: -100,
+    opacity: 0
+});
+
+gsap.to('#preloadpath-3', {
+    y: 0
+});
+
 function showHidePreloader() {
     preloader.classList.add('d-none');
 }
